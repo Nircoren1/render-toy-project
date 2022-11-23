9a3fc9f3e5c993b12ecfd18a92f594b37276442b
Nir <nircoren@gmail.com>
2022-11-23 18:24:55 +0200

    ASD

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const path = require('path')
 
 const toyService = require('./services/toy.service')
 
