6407df0b6187713b9ad65ec679ec50015f037138
Nir <nircoren@gmail.com>
2022-11-23 17:59:13 +0200

    removed detoy from modules

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ if (process.env.NODE_ENV === 'production') {
         'http://localhost:8080',
         'http://127.0.0.1:3000',
         'http://localhost:3000',
+        'http://127.0.0.1:5173',
+        'http://localhost:5173',
       ],
       credentials: true,
     }
